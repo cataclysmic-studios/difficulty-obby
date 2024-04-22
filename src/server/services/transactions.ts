9c1bb517cb9f56a65c80edb206bf5c55a019738a
R-unic <roleyspersonal@gmail.com>
2024-04-21 23:24:29 -0400

    feat: add coins dev products to TransactionsService

--- src/server/services/transactions.ts
+++ src/server/services/transactions.ts
@@ -6,13 +6,27 @@ import Log from "shared/logger";
 
 type RewardHandler = (player: Player) => void;
 const enum ProductIDs {
-  // Gold5000 = 1620635951,
+  Coins10000 = 1813571859,
+  Coins1750 = 1813571860,
+  Coins4000 = 1813571861,
+  Coins500 = 1813571862,
+  Coins25000 = 1813571863,
+  Coins250 = 1813571864,
+  Coins100 = 1813571865,
+  Coins1000 = 1813571866,
 }
 
 @Service()
 export class TransactionsService implements OnInit {
   private readonly rewardHandlers: Record<number, RewardHandler> = {
-    // [ProductIDs.Gold5000]: player => this.db.increment(player, "gold", 5000)
+    [ProductIDs.Coins100]: player => this.db.increment(player, "coins", 100),
+    [ProductIDs.Coins250]: player => this.db.increment(player, "coins", 250),
+    [ProductIDs.Coins500]: player => this.db.increment(player, "coins", 500),
+    [ProductIDs.Coins1000]: player => this.db.increment(player, "coins", 1000),
+    [ProductIDs.Coins1750]: player => this.db.increment(player, "coins", 1750),
+    [ProductIDs.Coins4000]: player => this.db.increment(player, "coins", 4000),
+    [ProductIDs.Coins10000]: player => this.db.increment(player, "coins", 10000),
+    [ProductIDs.Coins25000]: player => this.db.increment(player, "coins", 25000)
   }
 
   public constructor(
