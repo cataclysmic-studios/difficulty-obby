d1329e892ffcefe5aa0239efeb4ec37fe7a77b67
R-unic <roleyspersonal@gmail.com>
2024-04-21 21:20:15 -0400

    feat: create ToggleSwitch UI component

--- types/ReplicatedFirst.d.ts
+++ types/ReplicatedFirst.d.ts
@@ -1,5 +1,20 @@
 interface ReplicatedFirst extends Instance {
   Assets: Folder & {
-
+    UI: Folder & {
+      Setting: Frame & {
+        Title: TextLabel;
+      };
+      ToggleSwitch: ImageButton & {
+        UIPadding: UIPadding;
+        UICorner: UICorner;
+        UIStroke: UIStroke;
+        UIAspectRatioConstraint: UIAspectRatioConstraint;
+        Node: Frame & {
+          UICorner: UICorner;
+          UIStroke: UIStroke;
+          UIAspectRatioConstraint: UIAspectRatioConstraint;
+        };
+      };
+    };
   };
 }
