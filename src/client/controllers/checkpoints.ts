2a98856f93c36246b80f59dac10e5dce0a0c1e69
R-unic <roleyspersonal@gmail.com>
2024-04-22 18:06:33 -0400

    feat: create shell logic to prompt skip stage product purchase

--- src/client/controllers/checkpoints.ts
+++ src/client/controllers/checkpoints.ts
@@ -15,6 +15,7 @@ const { clamp } = math;
 export class CheckpointsController implements OnInit, LogStart {
   private stage = 0;
   private stageOffset = 0;
+  private firstTry = true;
 
   public constructor(
     private readonly character: CharacterController
@@ -26,9 +27,10 @@ export class CheckpointsController implements OnInit, LogStart {
     Events.data.updated.connect((directory, value) => {
       if (!endsWith(directory, "stage")) return;
       this.stage = <number>value;
+      this.firstTry = true;
 
       if (firstTime) {
-        this.respawn();
+        this.respawn(false);
         firstTime = false;
       }
     });
@@ -36,12 +38,12 @@ export class CheckpointsController implements OnInit, LogStart {
 
   public addStageOffset(offset = 1): void {
     this.stageOffset += offset;
-    this.respawn();
+    this.respawn(false);
   }
 
   public subtractStageOffset(offset = 1): void {
     this.stageOffset -= offset;
-    this.respawn();
+    this.respawn(false);
   }
 
   public getStageOffset(): number {
@@ -52,7 +54,7 @@ export class CheckpointsController implements OnInit, LogStart {
     return clamp(this.stage + this.stageOffset, 0, ZONE_INFO.size() * STAGES_PER_ZONE);
   }
 
-  private respawn(): void {
+  private respawn(promptSkip = true): void {
     const spawn = World.GetDescendants()
       .filter((i): i is SpawnLocation => i.IsA("SpawnLocation"))
       .find(spawn => spawn.Name === tostring(this.stage + this.stageOffset));
@@ -62,6 +64,18 @@ export class CheckpointsController implements OnInit, LogStart {
 
     const root = this.character.getRoot();
     if (root === undefined) return;
+    if (promptSkip && !this.firstTry) {
+      this.firstTry = false;
+      this.promptSkip();
+    }
+
+    if (promptSkip && this.firstTry)
+      this.firstTry = false;
+
     root.CFrame = spawn.CFrame.add(new Vector3(0, 6, 0));
+  }
+
+  private promptSkip(): void {
+    print("buy da skip stage product!!!!!11");
   }
 }
--- src/server/components/kill.ts
+++ src/server/components/kill.ts
@@ -7,10 +7,14 @@ import { Events } from "server/network";
 @Component({ tag: "KillPart" })
 export class KillPart extends BaseComponent<{}, BasePart> implements OnStart {
   public onStart(): void {
+    let debounce = false;
     this.instance.Touched.Connect(hit => {
       const character = hit.FindFirstAncestorOfClass("Model");
       const humanoid = character?.FindFirstChildOfClass("Humanoid");
       if (humanoid === undefined) return;
+      if (debounce) return;
+      debounce = true;
+      task.delay(0.25, () => debounce = false);
 
       const player = Players.GetPlayerFromCharacter(character);
       if (player === undefined) return;
