4a71b8b6cc3ce92257a639c1813fc5e44eb9c7bc
R-unic <roleyspersonal@gmail.com>
2024-04-22 22:52:33 -0400

    feat: play different ambience tracks per zone

--- src/client/controllers/atmosphere.ts
+++ src/client/controllers/atmosphere.ts
@@ -6,13 +6,20 @@ import { getZoneName } from "shared/constants";
 
 @Controller()
 export class AtmosphereController implements OnStart {
+  private currentAmbience?: Sound
+
   public async onStart(): Promise<void> {
     this.update(<number>await Functions.data.get("stage"))
   }
 
   public update(stage: number): void {
     const currentZone = getZoneName(stage);
-    const zoneLighting = World.Zones.WaitForChild(currentZone).WaitForChild("Lighting");
+    const zoneFolder = World.Zones.WaitForChild(currentZone);
+    const zoneLighting = zoneFolder.WaitForChild("Lighting");
+    this.currentAmbience?.Stop();
+    this.currentAmbience = <Sound>zoneFolder.WaitForChild("Ambience");
+    this.currentAmbience.Play();
+
     const oldLighting = Lighting.GetChildren().filter(i => !i.HasTag("DefaultLighting"));
     for (const lighting of oldLighting)
       lighting.Destroy();
