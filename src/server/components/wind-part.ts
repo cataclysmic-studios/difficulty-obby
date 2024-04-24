27f4995475f8eb1cf0a93b07d43f9bbccaf5d24a
R-unic <roleyspersonal@gmail.com>
2024-04-24 17:41:59 -0400

    feat: add vfx for WindPart

--- src/server/components/wind-part.ts
+++ src/server/components/wind-part.ts
@@ -2,6 +2,7 @@ import type { OnStart, OnTick } from "@flamework/core";
 import { Component, BaseComponent } from "@flamework/components";
 import { Players, SoundService as Sound } from "@rbxts/services";
 
+import { Assets } from "shared/utility/instances";
 import { removeDuplicates } from "shared/utility/array";
 
 interface Attributes {
@@ -31,6 +32,8 @@ export class WindPart extends BaseComponent<Attributes, BasePart> implements OnS
     collider.CFrame = this.instance.CFrame.add(this.instance.CFrame.LookVector.mul(this.attributes.WindPart_Distance / 2));
     collider.Parent = this.instance;
     this.collider = collider;
+
+    Assets.VFX.Wind.Clone().Parent = collider;
   }
 
   public onTick(dt: number): void {
