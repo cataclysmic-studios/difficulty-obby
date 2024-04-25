5a295424c36a3be05f3947534ca14a8a5933993d
R-unic <roleyspersonal@gmail.com>
2024-04-24 23:23:51 -0400

    feat: create SpinningPart component

---
+++ src/server/components/spinning-part.ts
@@ -0,0 +1,39 @@
+import type { OnTick } from "@flamework/core";
+import { Component, BaseComponent } from "@flamework/components";
+
+interface Attributes {
+  SpinningPart_Speed: number,
+  SpinningPart_Axis: "X" | "Y" | "Z";
+}
+
+@Component({
+  tag: "SpinningPart",
+  defaults: {
+    SpinningPart_Speed: 1,
+    SpinningPart_Axis: "Y"
+  }
+})
+export class SpinningPart extends BaseComponent<Attributes, BasePart> implements OnTick {
+  public onTick(dt: number): void {
+    const speed = this.attributes.SpinningPart_Speed * dt * 60;
+    let x = 0;
+    let y = 0;
+    let z = 0;
+    switch (this.attributes.SpinningPart_Axis) {
+      case "X": {
+        x = speed;
+        break;
+      }
+      case "Y": {
+        y = speed;
+        break;
+      }
+      case "Z": {
+        z = speed;
+        break;
+      }
+    }
+
+    this.instance.Orientation = this.instance.Orientation.add(new Vector3(x, y, z));
+  }
+}
