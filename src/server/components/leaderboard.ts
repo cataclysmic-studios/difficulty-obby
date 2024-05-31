8efcb2ecfc74d0afcaee78f9bc272b9f91ade42f
R-unic <roleyspersonal@gmail.com>
2024-05-30 21:42:07 -0400

    fix: only show top 100 players on leaderboard

--- src/server/components/leaderboard.ts
+++ src/server/components/leaderboard.ts
@@ -7,6 +7,10 @@ import { Assets } from "shared/utility/instances";
 import { commaFormat, toSuffixedNumber } from "shared/utility/numbers";
 
 import type { DatabaseService } from "server/services/third-party/database";
+import { slice } from "shared/utility/array";
+import Log from "shared/logger";
+
+const TOP_PLAYERS = 100;
 
 interface Attributes { }
 
@@ -17,29 +21,32 @@ export class Leaderboard extends BaseComponent<Attributes, BasePart> implements 
   ) { super(); }
 
   public onStart(): void {
-    const database = Object.entries(this.db.getDatabase())
-      .sort(([_, dataA], [__, dataB]) => dataA.stage > dataB.stage);
+    const database = slice(
+      Object.entries(this.db.getDatabase())
+        .sort(([_, dataA], [__, dataB]) => dataA.stage > dataB.stage)
+        .filter(([idString]) => tonumber(idString) !== undefined && tonumber(idString)! > 0),
+
+      0, TOP_PLAYERS
+    );
 
     const ui = Assets.UI.LeaderboardUI.Clone();
     ui.Adornee = this.instance;
     ui.Parent = this.instance;
 
-    for (const player of database)
-      task.spawn(() => {
-        const [idString, data] = player;
-        const index = database.indexOf(player);
-        const id = tonumber(idString)!;
-        if (id === undefined || id < 0) return;
+    for (const player of database) {
+      const [idString, data] = player;
+      const index = database.indexOf(player);
+      const id = tonumber(idString)!;
+      task.wait(0.2);
+      const [success, username] = pcall(() => Players.GetNameFromUserIdAsync(id));
+      if (!success) return Log.warning(<string>username);
 
-        const [success, username] = pcall(() => Players.GetNameFromUserIdAsync(id));
-        if (!success) return;
-
-        const entry = Assets.UI.LeaderboardEntry.Clone();
-        entry.Username.Text = username;
-        entry.Stage.Text = commaFormat(data.stage ?? 0);
-        entry.Coins.Text = toSuffixedNumber(data.coins ?? 0);
-        entry.LayoutOrder = index;
-        entry.Parent = ui.List;
-      });
+      const entry = Assets.UI.LeaderboardEntry.Clone();
+      entry.Username.Text = username;
+      entry.Stage.Text = commaFormat(data.stage ?? 0);
+      entry.Coins.Text = toSuffixedNumber(data.coins ?? 0);
+      entry.LayoutOrder = index;
+      entry.Parent = ui.List;
+    }
   }
 }
