8ad51a71e6360578397873f28e92c8ce4297eae1
R-unic <roleyspersonal@gmail.com>
2024-06-06 00:44:21 -0400

    fix: trails vanishing on death

--- src/server/services/trail.ts
+++ src/server/services/trail.ts
@@ -5,9 +5,14 @@ import { endsWith } from "@rbxts/string-utils";
 import type { OnDataUpdate } from "server/hooks";
 import type { LogStart, OnCharacterAdd } from "shared/hooks";
 import { Assets } from "shared/utility/instances";
+import { DatabaseService } from "./third-party/database";
 
 @Service()
 export class TrailService implements OnCharacterAdd, OnDataUpdate, LogStart {
+  public constructor(
+    private readonly db: DatabaseService
+  ) { }
+
   public onCharacterAdd(character: CharacterModel): void {
     const a0 = new Instance("Attachment", character.Torso);
     a0.Name = "Attachment0";
@@ -15,15 +20,21 @@ export class TrailService implements OnCharacterAdd, OnDataUpdate, LogStart {
     const a1 = new Instance("Attachment", character.Torso);
     a1.Name = "Attachment1";
     a1.Position = new Vector3(0, -1, 0);
+
+    this.update(Players.GetPlayerFromCharacter(character)!);
   }
 
-  public onDataUpdate(player: Player, directory: string, equippedTrail: Maybe<string>): void {
+  public onDataUpdate(player: Player, directory: string): void {
     if (!endsWith(directory, "equippedTrail")) return;
     if (player.Character === undefined && Players.GetPlayers().includes(player))
       player.CharacterAdded.Wait();
 
+    this.update(player);
+  }
+
+  private update(player: Player): void {
+    const equippedTrail = this.db.get<Maybe<string>>(player, "equippedTrail");
     const character = <CharacterModel>player.Character;
-    character.Torso.FindFirstChildOfClass("Trail")?.Destroy();
     const trail = Assets.CrateRewards.Trails.GetDescendants().find((i): i is Trail => i.IsA("Trail") && i.Name === equippedTrail)?.Clone();
     if (trail === undefined) return;
 
@@ -31,6 +42,7 @@ export class TrailService implements OnCharacterAdd, OnDataUpdate, LogStart {
     const a1 = <Attachment>character.Torso.WaitForChild("Attachment1", 5);
     if (a0 === undefined || a1 === undefined) return;
 
+    character.Torso.FindFirstChildOfClass("Trail")?.Destroy();
     trail.Parent = character.Torso;
     trail.Attachment0 = a0;
     trail.Attachment1 = a1;
